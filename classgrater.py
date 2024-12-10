01377fc4adff69656816be9142fc326e5a0a4fcf
Anasma123 <anasmapvt@gmail.com>
2024-12-10 02:24:27 -0800

    Add files via upload

--- classgrater.py
+++ classgrater.py
@@ -1,18 +1,30 @@
-class A:
+class Rectangle:
+    def __init__(self, length, breadth):
+        self.length = length
+        self.breadth = breadth
+
+    def area(self):
+        return self.length * self.breadth
+
+    def perimeter(self):
+        return 2 * (self.length + self.breadth)
+
+
+length1 = float(input("Enter the length of the first rectangle: "))
+breadth1 = float(input("Enter the breadth of the first rectangle: "))
+
+length2 = float(input("Enter the length of the second rectangle: "))
+breadth2 = float(input("Enter the breadth of the second rectangle: "))
+
+rect1 = Rectangle(length1, breadth1)
+rect2 = Rectangle(length2, breadth2)
+
+if rect1.area()>rect2.area():
+    print("1 is gtrt area",rect1.area(),"perimtr",rect1.perimeter())
+elif rect1.area()<rect2.area():
+    print("2 is grtr area",rect2.area(),"perimtr is",rect2.perimeter())
     
-    def __init__(self, a):
-        self.a = a
-        
-    def __gt__(self, other):  
-        if self.a > other.a:
-            return True
-        else:
-            return False
-
-ob1 = A(2)
-ob2 = A(3)
-
-if ob1 > ob2:  
-    print("ob1 is greater than ob2")
 else:
-    print("ob2 is greater than ob1")
+    print("both are equal rect one is",rect1.area(),rect1.perimeter(),"rect 2 is",rect2.area(),rect2.perimeter())
+    
+
